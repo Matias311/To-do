d822dc6234d195498397076f9b6c50e36741669b
Matias Arias <matiasarias3103@gmail.comm>
2023-09-11 20:28:11 -0300

    Eliminar una tarea

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const input = document.querySelector("[data-input]");
 const btn = document.querySelector("[data-btn]");
 const ul = document.querySelector("[data-card]");
+
 let array = [];
 import checkbox from "./components/check.js";
 import trashCan from "./components/trash.js";
+
+function reLoad() {
+  document.addEventListener("DOMContentLoaded", () => {
+    array = JSON.parse(localStorage.getItem("task")) || [];
+    crearHTML();
+  });
+}
+reLoad();
 
 function task() {
   let task = input.value;
   let data = {
     task: task,
-    id: Date.now(),
+    id: new Date().getTime(),
   };
   array = [...array, data];
 }
@@ -19,6 +28,7 @@ function crearHTML() {
   if (array.length > 0) {
     array.forEach((task) => {
       const li = document.createElement("li");
+      li.setAttribute("id", task.id);
       li.classList.add("card__todo");
       const title = document.createElement("h1");
       title.innerHTML = task.task;
@@ -40,13 +50,6 @@ function local() {
   localStorage.setItem("task", JSON.stringify(array));
 }
 
-function reLoad() {
-  document.addEventListener("DOMContentLoaded", () => {
-    array = JSON.parse(localStorage.getItem("task")) || [];
-    crearHTML();
-  });
-}
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   task();
@@ -55,4 +58,17 @@ btn.addEventListener("click", (e) => {
   local();
 });
 
-reLoad();
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("trash")) {
+    const taskId = e.target.closest("li").id;
+    console.log(taskId);
+    removeTask(taskId);
+  }
+});
+
+function removeTask(taskId) {
+  array = array.filter((task) => task.id !== parseInt(taskId));
+  local();
+  document.getElementById(taskId).remove();
+  console.log(array);
+}
